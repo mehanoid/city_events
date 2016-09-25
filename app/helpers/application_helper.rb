f3334afe4cf24b6a03d8ecfcfb300c93cc30284c
Oleg Grigoriev <mehanoid.ru@gmail.com>
2016-09-26 00:52:27 +0300

    cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
 module ApplicationHelper
-  def read_notifications
-    @notifications ||= begin
-      notifications = current_user.notifications.unread
-      notifications.update(read: true)
-      notifications
-    end
-  end
-
   def bootstrap_class_for flash_type
     {"success" => "alert-success", "error" => "alert-danger", "alert" => "alert-warning", "notice" => "alert-info"}[flash_type] || flash_type.to_s
   end
