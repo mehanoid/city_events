974efb439294ba42420ebdfff4c38838f59f19ff
Oleg Grigoriev <mehanoid.ru@gmail.com>
2016-09-26 00:50:38 +0300

    search by date fixes

--- app/queries/event_query.rb
+++ app/queries/event_query.rb
@@ -14,16 +14,16 @@ class EventQuery
       new_scope = new_scope.where(city: search_query.city)
     end
 
-    if search_query.name
+    if search_query.name.present?
       new_scope = new_scope.where('name ILIKE ?', "%#{search_query.name}%")
     end
 
     if search_query.from_date
-      new_scope = new_scope.where('ends_at <= ?', search_query.from_date.end_of_day)
+      new_scope = new_scope.where('ends_at >= ?', search_query.from_date.end_of_day)
     end
 
     if search_query.to_date
-      new_scope = new_scope.where('starts_at >= ?', search_query.from_date.beginning_of_day)
+      new_scope = new_scope.where('starts_at <= ?', search_query.to_date.beginning_of_day)
     end
 
     new_scope
